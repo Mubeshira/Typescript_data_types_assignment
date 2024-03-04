75039ad6b4eee7567db795563349e898d2aa64c7
Mubeshira <159130265+Mubeshira@users.noreply.github.com>
2024-03-04 14:26:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+// String example
+var personName = "Mubeshira";
+console.log("Hello," + personName);
+// Number example
+var rollNumber = 475625;
+console.log(rollNumber);
+//  boolean example
+var female = true;
+console.log(female);
+// null example
+var address = null;
+console.log(address);
+// undefined example
+var randomValue = undefined;
+console.log(randomValue);
---
+++ index.ts
@@ -0,0 +1,20 @@
+// String example
+let personName: string = "Mubeshira";
+console.log("Hello," + personName);
+
+
+// Number example
+let rollNumber: number = 475625 ;
+console.log(rollNumber);
+
+//  boolean example
+let female: boolean = true;
+console.log(female);
+
+// null example
+let address: null = null;
+console.log(address);
+
+// undefined example
+let randomValue: undefined = undefined;
+console.log(randomValue); 
